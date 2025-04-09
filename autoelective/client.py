8a40950e94b1310ef3cd6efcc973d6f6a9f6c77b
zcxxnqwq <2400012976@stu.pku.edu.cn>
2025-04-09 20:57:08 +0800

    Update client.py

--- autoelective/client.py
+++ autoelective/client.py
@@ -3,39 +3,53 @@
 # filename: client.py
 # modified: 2019-09-09
 
-from requests.models import Request
-from requests.sessions import Session
-from requests.cookies import extract_cookies_to_jar
+# request 第三方库
+from requests.models import Request #创建和准备 HTTP 请求
+from requests.sessions import Session #Session 对象使多个请求之间共享同一会话状态
+from requests.cookies import extract_cookies_to_jar #提取 HTTP 响应中的 cookies 并将其保存到 cookie jar 中（即一个 Python 对象，用于管理 cookies）
 
 class BaseClient(object):
 
     default_headers = {}
     default_client_timeout = 10
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs): #接收可变数量的位置/关键字参数
         if self.__class__ is __class__:
-            raise NotImplementedError
+            raise NotImplementedError #抛出异常 某方法/功能尚未实现
         self._timeout = kwargs.get("timeout", self.__class__.default_client_timeout)
         self._session = Session()
         self._session.headers.update(self.__class__.default_headers)
 
-    @property
-    def user_agent(self):
+    @property #将一个方法转变为属性
+    def user_agent(self): #浏览器或爬虫在每次请求时附带的一个 HTTP 头，告诉服务器客户端的类型和版本
         return self._session.headers.get('User-Agent')
 
     def _request(self, method, url,
             params=None, data=None, headers=None, cookies=None, files=None,
             auth=None, timeout=None, allow_redirects=True, proxies=None,
             hooks=None, stream=None, verify=None, cert=None, json=None):
-
+            #method：HTTP请求类型（GET/POST/...）
+            #url：请求的地址（网址）
+            #params：url查询参数params = {'q': 'python', 'page': 2} URL会变成 https://example.com/search?q=python&page=2
+            #data：发送的表单数据，data = {'username': 'myuser', 'password': 'mypassword'}
+            #headers：包含请求额外信息的字典，User-Agent（浏览器信息）、Content-Type（数据类型）、Authorization（身份验证）等
+            #cookies：cookies = {'session_id': '1234567890'}
+            #files：用来上传文件，通常用于 POST 请求
+            #auth：用来进行基本的HTTP认证的元组 (username, password)
+            #allow_redirects：是否允许自动跟随HTTP重定向
+            #proxies：指定通过代理服务器发送请求，proxies = {'http': 'http://myproxy.com', 'https': 'https://myproxy.com'}
+            #hooks：用来设置在请求和响应过程中触发的回调函数
+            #stream：是否以流的形式处理响应。默认一次性下载整个响应。如果 stream=True，响应体会被分块接收，可以处理大型文件下载或上传
+            #cert：用于设置客户端证书
+            #json：用于直接发送JSON数据，json = {"key": "value"}
         # Extended from requests/sessions.py  for '_client' kwargs
 
         req = Request(
-            method=method.upper(),
+            method=method.upper(), #转大写字符
             url=url,
             headers=headers,
             files=files,
-            data=data or {},
+            data=data or {}, #data 有值则data，否则空
             json=json,
             params=params or {},
             auth=auth,
@@ -62,10 +76,10 @@ class BaseClient(object):
 
         return resp
 
-    def _get(self, url, params=None, **kwargs):
+    def _get(self, url, params=None, **kwargs): #请求获取数据
         return self._request('GET', url,  params=params, **kwargs)
 
-    def _post(self, url, data=None, json=None, **kwargs):
+    def _post(self, url, data=None, json=None, **kwargs): #请求提交数据
         return self._request('POST', url, data=data, json=json, **kwargs)
 
     def set_user_agent(self, user_agent):
